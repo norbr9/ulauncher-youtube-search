1dafdca7c410f70a125b5b036086c90441608a14
Henri Kestiö <henri.kestio@gmail.com>
2021-06-27 19:10:32 +0300

    Overhaul youtube search execution

--- main.py
+++ main.py
@@ -2,25 +2,23 @@ import logging
 import gi
 gi.require_version('Gdk', '3.0')
 
-from youtubeSearch import YoutubeSearch
+from youtube_search import YoutubeSearch
+from items import generate_search_items, no_input_item, no_results_item
 
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
-from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
 
 
 logger = logging.getLogger(__name__)
 
-icon_file='images/icon.png'
 
-
-class YoutubeSeachExtension(Extension):
+class YoutubeSearchExtension(Extension):
     def __init__(self):
-        super(YoutubeSeachExtension, self).__init__()
+        super(YoutubeSearchExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
 
 
@@ -29,34 +27,15 @@ class KeywordQueryEventListener(EventListener):
         query = event.get_argument() or str()
 
         if len(query.strip()) == 0:
-            return RenderResultListAction([
-                ExtensionResultItem(
-                    icon=icon_file,
-                    name='No input',
-                    on_enter=HideWindowAction()
-                )
-            ])
+            return RenderResultListAction(no_input_item())
 
-        result_dict = YoutubeSearch.perform_search(query)
+        result_dict = YoutubeSearch.execute(query)
         logger.debug('Obtained result' + str(result_dict))
 
-        
         if not result_dict:
-             return RenderResultListAction([
-                ExtensionResultItem(
-                    icon=icon_file,
-                    name='No results',
-                    on_enter=HideWindowAction()
-                )
-            ])
+            return RenderResultListAction(no_results_item())
 
-
-        return RenderResultListAction([ ExtensionResultItem(
-                icon=icon_file,
-                name=title,
-                description='',
-                on_enter= OpenUrlAction("https://youtube.com" + url))
-                for title, url in result_dict.items() ])
+        return RenderResultListAction(generate_search_items(result_dict, extension.preferences['description_template']))
 
 if __name__ == "__main__":
-    YoutubeSeachExtension().run()
+    YoutubeSearchExtension().run()
---
+++ youtube_search.py
@@ -0,0 +1,62 @@
+import urllib.parse
+import requests
+import json
+
+from functions import clear_thumbnails, save_thumbnail
+
+
+MAX_RESULTS = 10
+
+SEARCH_BASE_URL = 'https://youtube.com/results?search_query='
+
+BASE_URL = 'https://youtube.com'
+
+
+class YoutubeSearch():
+    def __init__(self, params):
+        self.show_thumbnails = None
+        self.query = ' '.join(params)
+
+        clear_thumbnails()
+
+
+    def execute(self):
+        response = requests.get(SEARCH_BASE_URL + urllib.parse.quote(self.query)).text
+        results = []
+        start = (
+            response.index('ytInitialData') + len('ytInitialData') + 3
+        )
+        end = response.index('};', start) + 1
+
+        data = json.loads(response[start:end])
+
+        contents = data['contents']['twoColumnSearchResultsRenderer']['primaryContents']['sectionListRenderer']['contents']
+
+        for content in contents:
+            items = content.get('itemSectionRenderer', {}).get('contents', {})
+
+            for item in items:
+                if len(results) > MAX_RESULTS:
+                    return results
+
+                if 'videoRenderer' in item.keys():
+                    video_data = item['videoRenderer']
+                    video_id = video_data.get('videoId', None)
+                    thumbnail = video_data.get('thumbnail', {}).get('thumbnails', [{}])[0].get('url', None)
+                    thumbnail_path = None
+
+                    if self.show_thumbnails and thumbnail:
+                        thumbnail_path = save_thumbnail(thumbnail, video_id)
+
+                    results.append({
+                        'id': video_id,
+                        'thumbnail': thumbnail_path,
+                        'title': video_data.get('title', {}).get('runs', [{}])[0].get('text', '').replace('&', 'and'),
+                        'channel': video_data.get('longBylineText', {}).get('runs', [{}])[0].get('text', None),
+                        'date': video_data.get('publishedTimeText', {}).get('simpleText'),
+                        'duration': video_data.get('lengthText', {}).get('simpleText', 0),
+                        'views': video_data.get('viewCountText', {}).get('simpleText', 0),
+                        'url': BASE_URL + video_data.get('navigationEndpoint', {}).get('commandMetadata', {}).get('webCommandMetadata', {}).get('url', None)
+                    })
+
+        return results
